b3d439d19bfa7f855c6cc08b02325776dbe1d6e6
James R. Carr (esquire) <james.r.carr@gmail.com>
2014-09-30 21:04:18 +0000

    path fixin

--- start-graylog2-server.sh
+++ start-graylog2-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # this is to give supervisor time to start mongo because graylog2-server
 # does not retry connections (yet)
-sleep 30 && /usr/bin/java -jar /opt/graylog2-server/graylog2-server.jar
+sleep 30 && /usr/bin/java -jar /usr/share/graylog2-server/graylog2-server.jar
